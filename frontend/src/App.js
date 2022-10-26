9290fbe28616297355e0860b2f6040822e0e338c
Leah Howells <65057296+leahhowells@users.noreply.github.com>
2022-10-26 16:28:25 -0400

    Add bootstrap and begin login page work
    Install bootstrap, edit app.js and app.css with new page.js component. Add background.jpg to use as website background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import Page from './Page';
 import './App.css';
+import background from "./background.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className="background" style={{ backgroundImage: `url(${background})` }}>
+      <div className="container">
+        <Page />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ frontend/src/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Page extends Component {
+
+    render() {
+      return (
+          <div id="outer-rectangle">
+            <div id="inner-rectangle">
+            </div>
+          </div>
+      );
+    }
+
+}
