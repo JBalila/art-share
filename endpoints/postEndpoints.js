2851b6bb19f38e55740c5204d22f23a7fbc2b898
ryanpenland <91754520+ryanpenland@users.noreply.github.com>
2022-11-02 18:04:24 -0400

    Update postEndpoints.js

--- endpoints/postEndpoints.js
+++ endpoints/postEndpoints.js
@@ -5,5 +5,12 @@ const jwt = require('../createJWT');
 const Post = require('../models/Post');
 
 exports.setPostEndpoints = function(app, client) {
+    app.post('/api/feed', async(req, res, next) => {
+        
+        const { clique } = req.body;
 
+        let postArray = await Post.find().or([{isPublic: true}, {AuthorId: {$in: clique}}]);
+        
+        res.status(200).json(postArray);
+    })
 }
