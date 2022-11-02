031b56f0bf53d269827a211c0e96dad29a06108a
JBalila <91769667+JBalila@users.noreply.github.com>
2022-11-02 17:42:09 -0400

    Hooked up API endpoints to Login/Register pages

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,40 +1,82 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import Page from '../components/Page';
+import bp from '../components/Path';
 import '../LoginRegisterPage.css';
 
 import background from "../background.jpg";
 
 function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const login = async function() {
+    if (username.trim() === '' || password.trim() === '') {
+      setError('Please fill in all fields');
+      return;
+    }
+
+    let obj = {username: username, password: password};
+    let jsonPayload = JSON.stringify(obj);
+
+    try {
+      const response = await fetch(bp.buildPath('/api/login'), {
+        method:'POST', body:jsonPayload, headers: {
+          'Content-Type':'application/json'
+        }
+      });
+
+      let res = JSON.parse(await response.text());
+      if (res.error) {
+        setError(res.error);
+        return;
+      }
+
+      setError('');
+      // TODO: Store user-data into local storage
+      // TODO: Store access-token into local storage
+
+      window.location.href='/profile';
+    }
+    catch(e) {
+      console.error(e);
+    }
+  }
+
   return(
     <div className="background" style={{ backgroundImage: `url(${background})` }}>
         <Page className="leftPage">
-          <div class="page-format">
-            <div class="title">
+          <div className="page-format">
+            <div className="title">
             Welcome <br/>
             To <br/>
-            <div class="title-color"><b>Art Share</b></div>
+            <div className="title-color"><b>Art Share</b></div>
             </div>
           </div>
         </Page>
 
         <Page className="rightPage">
-          <div class="page-format">
-            <div class="title">Log In</div>
+          <div className="page-format">
+            <div className="title">Log In</div>
 
-            <div class="input-format">
-              <label class="label" for="Username">Username</label>
-              <input type="text" class="form-control" id="Username" placeholder="Username"></input>
-              <label class="label" for="Password">Password</label>
-              <input type="text" class="form-control" id="Password" placeholder="Password"></input>
-            </div>
+            <div className="input-format">
+              <label className="label" htmlFor="Username">Username</label>
+              <input type="text" className="form-control" id="Username" placeholder="Username" 
+                value={username} onChange={(e) => setUsername(e.target.value)} />
+              <label className="label" htmlFor="Password">Password</label>
+              <input type="password" className="form-control" id="Password" placeholder="Password" 
+                value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div> <br />
+            <span>{error}</span>
 
-              <button class="button">
+              <button className="button" onClick={login}>
               Log In
               </button>
 
-            <div class="footer">
+            <div className="footer">
               <Link to="/register">Register Account</Link>
               <br/>
               <Link to="/">Forgot Password</Link>
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,44 +1,99 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import Page from '../components/Page';
+import bp from '../components/Path';
 import '../LoginRegisterPage.css';
 
 import background from "../background.jpg";
 
 function RegisterPage() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const register = async function() {
+    if (email.trim() === '' || username.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
+      setMessage('Please fill in all fields');
+      return;
+    }
+
+    // TODO: Check if email is even valid (has an @ and a .com)
+
+    if (password !== confirmPassword) {
+      setMessage('Your passwords do not match');
+      return;
+    }
+
+    let obj = {
+      firstName: 'Test',
+      lastName: 'Test',
+      email: email,
+      username: username,
+      password: password
+    };
+    let jsonPayload = JSON.stringify(obj);
+
+    try {
+      const response = await fetch(bp.buildPath('/api/register'), {
+        method:'POST', body:jsonPayload, headers: {
+          'Content-Type':'application/json'
+        }
+      });
+
+      let res = JSON.parse(await response.text());
+      if (res.error) {
+        setMessage(res.error);
+        return;
+      }
+
+      setMessage('A confirmation link has been sent to your email');
+    }
+    catch(e) {
+      console.error(e);
+    }
+  }
+
   return(
     <div className="background" style={{ backgroundImage: `url(${background})` }}>
         <Page className="leftPage">
-          <div class="page-format">
-            <div class="title">
+          <div className="page-format">
+            <div className="title">
             Welcome <br/>
             To <br/>
-            <div class="title-color"><b>Art Share</b></div>
+            <div className="title-color"><b>Art Share</b></div>
             </div>
           </div>
         </Page>
 
         <Page className="rightPage">
-          <div class="page-format">
-            <div class="title">Register</div>
+          <div className="page-format">
+            <div className="title">Register</div>
 
-            <div class="input-format">
-              <label class="label" for="Email">Email</label>
-              <input type="text" class="form-control" id="Email" placeholder="Email"></input>
-              <label class="label" for="Username">Username</label>
-              <input type="text" class="form-control" id="Username" placeholder="Username"></input>
-              <label class="label" for="Password">Password</label>
-              <input type="text" class="form-control" id="Password" placeholder="Password"></input>
-              <label class="label"  for="ConfirmPassword">Confirm Password</label>
-              <input type="text" class="form-control" id="ConfirmPassword" placeholder="Confirm Password"></input>
-            </div>
+            <div className="input-format">
+              <label className="label" htmlFor="Email">Email</label>
+              <input type="text" className="form-control" id="Email" placeholder="Email" 
+                value={email} onChange={(e) => setEmail(e.target.value)} />
+              <label className="label" htmlFor="Username">Username</label>
+              <input type="text" className="form-control" id="Username" placeholder="Username" 
+                value={username} onChange={(e) => setUsername(e.target.value)} />
+              <label className="label" htmlFor="Password">Password</label>
+              <input type="password" className="form-control" id="Password" placeholder="Password" 
+                value={password} onChange={(e) => setPassword(e.target.value)} />
+              <label className="label" htmlFor="ConfirmPassword">Confirm Password</label>
+              <input type="password" className="form-control" id="ConfirmPassword" placeholder="Confirm Password" 
+                value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+            </div> <br />
+            <span>{message}</span>
 
-              <button class="button">
+              <button className="button" onClick={register}>
               Register
               </button>
 
-            <div class="footer">
+            <div className="footer">
               <Link to="/">Go to Log In</Link>
             </div>
 
