3689757f77c1594b2b2babc23cd3d20a72d1ed11
Leah Howells <65057296+leahhowells@users.noreply.github.com>
2022-11-02 14:21:46 -0400

    Jhamori Register Page Code

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,17 +1,64 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import Page from '../components/Page';
+import '../RegisterPage.css';
 
 import background from "../background.jpg";
 
 function RegisterPage() {
   return(
-    <div className="background" style={{ backgroundImage: `url(${background})` }}>
-      <Page classname='leftpage'>
-      </Page>
-      <Page classname='rightpage'>
-      </Page>
-    </div>
+      <div className="background" style={{ backgroundImage: `url(${background})` }}>
+          <Page className="leftPage">
+          <div class="leftpage-format">
+              <p>
+              Welcome <br>
+              </br>
+              To <br>
+              </br>
+              <div class="pink">
+              Art Share
+              </div>
+
+              </p>
+            </div>
+          </Page>
+          <Page className="rightPage">
+            <div class="leftpage-format">
+              Register
+            </div>
+            <div >
+                Email
+                <br></br>
+                <input type="text" class="form-control" id="Email" placeholder="Email"></input>
+            </div>
+            <div>
+            Username
+            <br></br>
+            <input type="text" class="form-control" id="UserName" placeholder="UserName"></input>
+          </div>
+          <div>
+            Password
+            <br></br>
+            <input type="text" class="form-control" id="Password" placeholder="Password"></input>
+          </div>
+          <div>
+            Confirm Password
+            <br></br>
+            <input type="text" class="form-control" id="ConfirmPassword" placeholder="Confirm Password"></input>
+          </div>
+          <div class="parent">
+          <button class="bpink parent">
+            Register
+          </button>
+          </div>
+          <div class="parent">
+          <a href='#'>Login Page</a>
+          </div>
+
+          </Page>
+
+      </div>
   )
 }
 
